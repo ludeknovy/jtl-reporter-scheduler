e1b7fbeaf400542c98f7b32faf3201babddbe9c8
Ludek Novy <13610612+ludeknovy@users.noreply.github.com>
2022-11-15 08:34:26 +0100

    do not delete base run during housekeeping task (#6)

--- jobs/test-run-housekeeping.ts
+++ jobs/test-run-housekeeping.ts
@@ -4,7 +4,8 @@ const PG_DEFAULT_PORT = 5432
 const DELETE_QUERY = `DELETE FROM jtl.items WHERE id IN (
         SELECT it.id FROM jtl.items it LEFT JOIN jtl.scenario sc on it.scenario_id = sc.id
         WHERE sc.keep_test_runs_period > 0
-        AND it.start_time < now() - interval '1 day' * sc.keep_test_runs_period) 
+        AND it.start_time < now() - interval '1 day' * sc.keep_test_runs_period
+        AND it.base IS NOT TRUE) 
     RETURNING id, start_time`
 
 export const client = new Client({
